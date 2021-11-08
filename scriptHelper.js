1387054f4c588d1bacf6ab16eefb1893004a322e
Sandhya Gurrala <g.sandhya2011@gmail.com>
2021-11-08 15:18:56 -0600

    worked again

--- scriptHelper.js
+++ scriptHelper.js
@@ -44,19 +44,7 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    // let form = document.getElementById("testForm");
-    // form.addEventListener("submit", function(event){
-    //     let pilotName = document.querySelector("input[name = pilotName]");
-    //     let copilotName = document.querySelector("input[name = copilotName]");
-    //     let fuelLevel = document.querySelector("input[name = fuelLevel]");
-    //     let cargoMass = document.querySelector("input[name = cargoMass]")
-
-    //     if(pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass === ""){
-    //         alert("All fields are required!");
-    //         event.preventDefault();
-    //     }
-
-    // });
+    
     
 }
 
